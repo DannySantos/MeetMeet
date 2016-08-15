e57586c4c085bf70e6216481770f72b002e8c1b3
Danny Santos <danny@ethicalswitch.com>
2016-08-15 18:13:38 +0100

    Log In feature

--- features/step_definitions/users_steps.rb
+++ features/step_definitions/users_steps.rb
@@ -3,11 +3,36 @@ Given(/^they are on the registration page$/) do
   click_on "Register"
 end
 
+Given(/^a User has previously registered$/) do
+  @user = User.create!(
+    {
+      email: "test@test.com",
+      password: "password",
+      password_confirmation: "password",
+      confirmed_at: Date.new
+    }
+  )
+end
+
+Given(/^they are on the log in page$/) do
+  visit new_user_session_path
+end
+
 When(/^they fill in the registration form with valid details$/) do
   fill_in "Email", with: "test@test.com"
   fill_in "Password", with: "password"
   fill_in "Password confirmation", with: "password"
   click_button "Sign up"
+end
+
+When(/^they fill out the log in form with valid details$/) do
+  fill_in "Email", with: "test@test.com"
+  fill_in "Password", with: "password"
+  click_button "Log in"
+end
+
+Then(/^they are logged in$/) do
+  expect(page).to have_content("Signed in successfully.")
 end
 
 Then(/^a User should be created$/) do
