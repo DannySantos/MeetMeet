3a29f594e5e4250354e061ded77f4f83ea6870cb
Danny Santos <danny@ethicalswitch.com>
2016-08-16 09:29:38 +0100

    Bug fix: Fix confirmable for production

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,6 +8,9 @@ Devise.setup do |config|
   # by default. You can change it below and use your own secret key.
   # config.secret_key = 'ddd26204f592f3a750889040be66d52cb87680a6798af456b9a45770c64ff6f55e0a34af9c3d878e4ed19e32f8ce857eff1cd958978e7dc327ccdf2072e5f311'
 
+#  config.secret_key = ENV[ 'DEVISE_TOKEN_AUTH_SECRET_KEY' ]
+  
+  
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
