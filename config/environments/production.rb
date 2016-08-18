fcad25b3928acb116da800d3952c92fbe7e3c526
Danny Santos <danny@ethicalswitch.com>
2016-08-18 13:19:25 +0100

    Fix confirmable for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { :host => 'floating-ridge-96861.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'meetmeet.herokuapp.com/' }
+  
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 end
