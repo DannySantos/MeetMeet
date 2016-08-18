897e9490a272497f58a2aec4ff1ade4c9a798532
Danny Santos <danny@ethicalswitch.com>
2016-08-18 12:40:41 +0100

    Heroku bug fix

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,7 +1,7 @@
 class ConfirmationsController < Devise::ConfirmationsController
   private
   
-  def after_confirmation_path_for(User, resource)
+  def after_confirmation_path_for(:user, resource)
     "/hi"
   end
 end
