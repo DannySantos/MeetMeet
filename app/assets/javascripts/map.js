ec26634b5c6cc60ff09d590af9016f5ecd795503
Danny Santos <danny@ethicalswitch.com>
2016-08-19 11:47:02 +0100

    A visitor sees search results on a map   * Added Google Map   * Added AJAX call for requests   * Added markers

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,52 @@
+var map;
+var requestsList;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 51.397, lng: -0.644},
+    zoom: 8
+  });
+  
+  requestsList = [];
+  
+  loadRequests();
+  
+  map.addListener('bounds_changed', function() {
+    $('#search-results').empty();
+    
+    for(i = 0; i < requestsList.length; i++) {
+      var request = requestsList[i];
+      
+      if(map.getBounds().contains(request.marker.getPosition())) {
+        $('#search-results').append(
+          "<a href=\"/profiles/" + request.request.profile_id + "\">" +
+            "<h3>" + request.request.profile_id + "</h3>" + 
+          "</a>"
+        );
+      }
+    }
+  });
+}
+
+loadRequests = function() {
+  $.ajax({
+    type: 'GET',
+    url: "/requests/get_requests_json",
+    contentType: "application/json",
+    dataType: "json",
+    success: function(requests) {
+      for(var i = 0; i < requests.requests.length; i++) {
+        addMarker(requests.requests[i]);
+      }
+    }
+  })
+}
+
+addMarker = function(request) {
+  var marker = new google.maps.Marker({
+    position: { lat: request.latitude, lng: request.longitude },
+    map: map
+  });
+  
+  requestsList.push({marker: marker, request: request});
+}
--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,4 +2,10 @@ class RequestsController < ApplicationController
   def search
     @requests = Request.search(conditions: {activity_id: params[:search][:activity_id], town_country: params[:search][:town_country], country: params[:search][:country]})
   end
+  
+  def get_requests_json
+    @requests = Request.all
+    
+    render json: {requests: @requests}
+  end
 end
